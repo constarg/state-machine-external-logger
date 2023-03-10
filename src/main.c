48478623f4f108069d4cd2e23c3a2c97d5b29ab4
embeddedcat <embeddedcat@pm.me>
2023-03-11 01:36:25 +0200

    Done with the basic structure.

--- src/main.c
+++ src/main.c
@@ -7,39 +7,96 @@
 #define GPIOS_LEN 11 // the number of gpios.
 
 
+static inline void log_checker()
+{
+    uint8_t checker_state = 0;
+    // get all bits.
+    checker_state |= gpio_get(CHECKER_BIT0) << 0;
+    checker_state |= gpio_get(CHECKER_BIT1) << 1;
+    checker_state |= gpio_get(CHECKER_BIT2) << 2;
 
+    // TODO - check the checker state and log the state.
+}
 
-void state_logger_gpio_isr(uint gpio, uint32_t event_mask) {
-    printf("%d\n", gpio);
+static inline void log_core_standbywfe()
+{
+    uint8_t core_standbywfe_status = 0;
+    // get all bits.
+    core_standbywfe_status |= gpio_get(CORE_STANDBYWFE_BIT0) << 0;
+    core_standbywfe_status |= gpio_get(CORE_STANDBYWFE_BIT1) << 1;
+
+    // TODO - check the status.
+}
+
+static inline void log_core_parity_err() 
+{
+    uint8_t core_parity_err = 0;
+    // get all bits
+    core_parity_err |= gpio_get(CORE_PARITY_ERR_BIT0) << 0;
+    core_parity_err |= gpio_get(CORE_PARITY_ERR_BIT1) << 1;
+
+    // TODO - check the status
+}
+
+static inline void log_watchdog_timer_ex() 
+{
+    uint8_t watchdog_timer_ex = 0;
+    // get all bits.
+    watchdog_timer_ex = gpio_get(WATCHDOG_TIMER_EX_BIT);
+
+    // TODO - check the status.
+}
+
+static inline void log_forced_standby() 
+{
+    uint8_t forced_standby_status = 0;
+    forced_standby_status |= gpio_get(FORCED_STANDBY_BIT0) << 0;
+    forced_standby_status |= gpio_get(FORCED_STANDBY_BIT1) << 1;
+    // TODO - check the status.
+}
+
+static inline void log_checkpoint_err_after_Bt()
+{
+    uint8_t checkpoint_err_after_bt = 0;
+    checkpoint_err_after_bt = gpio_get(CHECKPOINT_ERR_AFTER_BT_BIT);
+
+    // TODO - check the bit.
+}
+
+static void state_logger_gpio_isr(uint gpio, uint32_t event_mask) {
     if (IS_CHECKER(gpio)) {
-        printf("CHECKER\n");
+        log_checker();
     } else if (IS_CORE_STANDBYWFE(gpio)) {
-        printf("CORE SDB\n");
+        log_core_standbywfe();
     } else if (IS_CORE_PARITY_ERR(gpio)) {
-        printf("CORE PARITY\n");
+        log_core_parity_err();
     } else if (IS_WATCHDOG_TIMER_EX(gpio)) {
-        printf("WATCHDOG\n");
+        log_watchdog_timer_ex();
     } else if (IS_FORCED_STANDBY(gpio)) {
-        printf("FORCED\n");
+        log_forced_standby();
     } else if (IS_CHECKPOINT_ERR_AFTER_BT(gpio)) {
-        printf("CHECKPOINT\n");
+        log_checkpoint_err_after_BT();
     }
 }
 
 // Initialize all the gpios to be used.
 static void state_logger_init_gpios()
 {
+    sleep_ms(5000);
     for (int gpio = 0; gpio < GPIOS_LEN; gpio++) {
-        printf("OK -> %d\n", gpio);
         // initilize current gpio
         gpio_init(gpio);
         // set current gpio to input mode.
         gpio_set_dir(gpio, GPIO_IN);
         // enable interrups for each bpio.
-        gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | 
-                                                 GPIO_IRQ_EDGE_FALL, 
-                                           true, 
-                                           &state_logger_gpio_isr);
+        if (0 == gpio) {
+            gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | 
+                                                     GPIO_IRQ_EDGE_FALL, 
+                                                     true, 
+                                                     &state_logger_gpio_isr);
+        } else {
+            gpio_set_irq_enabled(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+        }
     }
 }
 
@@ -48,18 +105,6 @@ int main(void)
     stdio_init_all();
 
     state_logger_init_gpios();
-    //
-         //   gpio_init(2);
-        // set current gpio to input mode.
-        //gpio_set_dir(2, GPIO_IN);
-        // enable interrups for each bpio.
-        /*gpio_set_irq_enabled_with_callback(2, GPIO_IRQ_EDGE_RISE, 
-                                           true, 
-                                           &gpio_isr);*/
 
-
-    while (true) {
-        //printf("ALL OK\n");
-        //sleep_ms(2000);
-    }
+    while (true);
 }
