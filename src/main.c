69352899fac2cf09379de3b88a4ebcb645ad8e6d
embeddedcat <embeddedcat@pm.me>
2023-03-18 13:59:26 +0200

    measure the time of the ISR

--- src/main.c
+++ src/main.c
@@ -4,7 +4,10 @@
 #include "pico/stdlib.h"
 
 #include "pico/sync.h"
+#include "hardware/clocks.h"
+#include "hardware/structs/systick.h"
 
+#include <inttypes.h>
 
 #define GPIOS_LEN 11 // the number of gpios.
 
@@ -44,17 +47,24 @@ static void state_logger_gpio_isr(uint gpio, uint32_t event_mask)
 {
     signal_buff signals = 0;
 
-    uint8_t intr_state = 0;
-
     // read signals.
     read_signals(&signals);
 
+    uint8_t intr_state = 0;
     intr_state = save_and_disable_interrupts();
+
+/*    systick_hw->cvr = 0x0;
+    systick_hw->rvr = 0x00ffffff;
+    systick_hw->csr = 0x5;*/
+//    uint32_t start = systick_hw->cvr; // start
+
     if (g_prev_signals == signals) {
         restore_interrupts(intr_state);
         return;
     } else {
         g_prev_signals = signals;
+//        uint32_t end = systick_hw->cvr;
+//        printf("Nanosecods: %d\n", (start - end) * 8);
         restore_interrupts(intr_state);
         print_signals(signals, time_us_32());
     }
@@ -82,5 +92,6 @@ int main(void)
 {
     stdio_init_all();
     state_logger_init_gpios();
+
     while (true);
 }
