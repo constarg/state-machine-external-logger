851e28dd021d3e79fd665ae53d45039ee90338ba
embeddedcat <constarg@pm.me>
2023-03-22 19:57:43 +0200

    optimize code

--- src/main.c
+++ src/main.c
@@ -11,29 +11,18 @@
 
 #define GPIOS_LEN 11 // the number of gpios.
 
-typedef uint16_t signal_buff; // The buffer in which we store the signals.
 
-static signal_buff g_prev_signals = 0; // The previous signals.
+static uint32_t signal_buff[200] = {0}; // The queue of signals.
+static volatile int8_t signal_index = -1; // Where to store the next signal.
 
-/**
- * This function read the value of all GPIOS_LEN 
- * gpios and store each bit in the dst.
- * 
- * @param dst Where the bits of all GPIOS_LEN gpios will be saved.
- */
-static inline void read_signals(signal_buff *dst)
-{
-    for (int sig = 0; sig < GPIOS_LEN; sig++) {
-        *dst |= gpio_get(sig) << sig;
-    }
-}
+static uint32_t prev_signal = 0;
 
 /**
  * This function print the value of each signal.
  * 
  * @param src The signal buffer to print.
  */
-static inline void print_signals(signal_buff src, uint32_t timestamp)
+static inline void print_signals(uint32_t src, uint32_t timestamp)
 {
     printf("%d ", timestamp);
     for (int sig = 0; sig < GPIOS_LEN; sig++) {
@@ -45,29 +34,22 @@ static inline void print_signals(signal_buff src, uint32_t timestamp)
 // Interrupt service routine for the gpios.
 static void state_logger_gpio_isr(uint gpio, uint32_t event_mask) 
 {
-    signal_buff signals = 0;
-
-    // read signals.
-    read_signals(&signals);
-
     uint8_t intr_state = 0;
+    signal_index += 1;
+
     intr_state = save_and_disable_interrupts();
-
-/*    systick_hw->cvr = 0x0;
+    systick_hw->cvr = 0x0;
+    systick_hw->csr = 0x5;
     systick_hw->rvr = 0x00ffffff;
-    systick_hw->csr = 0x5;*/
-//    uint32_t start = systick_hw->cvr; // start
+    
+    uint32_t start = systick_hw->cvr; // start
 
-    if (g_prev_signals == signals) {
-        restore_interrupts(intr_state);
-        return;
-    } else {
-        g_prev_signals = signals;
-//        uint32_t end = systick_hw->cvr;
-//        printf("Nanosecods: %d\n", (start - end) * 8);
-        restore_interrupts(intr_state);
-        print_signals(signals, time_us_32());
-    }
+    // Get the values of all 11 gpios. Ignore the others using the mask 0x7FF.
+    signal_buff[signal_index] = gpio_get_all();
+    uint32_t end = systick_hw->cvr;
+
+    printf("%u\n", (start - end) * 8);
+    restore_interrupts(intr_state);
 }
 
 // Initialize all the gpios to be used.
@@ -88,10 +70,21 @@ static void state_logger_init_gpios()
     }
 }
 
+
 int main(void)
 {
+    uint32_t curr_signal = 0;
+
     stdio_init_all();
     state_logger_init_gpios();
 
-    while (true);
+    while (true) {
+        if (signal_index > -1) {
+            curr_signal = signal_buff[signal_index];
+            signal_index -= 1;
+            if (curr_signal == prev_signal) continue;
+            prev_signal = curr_signal;
+            print_signals(curr_signal, time_us_32());
+        }
+    }
 }
