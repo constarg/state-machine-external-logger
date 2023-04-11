c5568d2582d700f8f82f119f0acc5ae64c2ad03b
embeddedcat <constarg@pm.me>
2023-04-11 15:25:32 +0300

    Fix duplicate issue

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include "hardware/clocks.h"
 // GPIO handler assembly.
 #include "gpio_handler.pio.h"
 
@@ -21,7 +20,7 @@ static inline void print_signals(uint32_t src)
 {
     // reposition the signal order to much the specification.
     uint8_t first_byte = src & 0xFF;
-    uint8_t second_byte = src << 8;
+    uint8_t second_byte = src >> 8;
 
     printf("%c", first_byte);
     printf("%c", second_byte);
@@ -36,8 +35,12 @@ int main(void)
     uint32_t prev_signals = 0; // The previous values of the GPIOS.
     uint8_t shift_amount = 32 - GPIOS_LEN; // How many bits to shift right.
 
+    bool checker_chg = 0; // Determines if the checker has changed.
+    bool rest_signals_chg = 0; // Determines if any other signal after the first 5 bits has changed.
+
     stdio_init_all();
 
+    // increase the system clock to it's maximum potentials.
     set_sys_clock_khz(133000, true);
     
     offset = pio_add_program(pio, &gpio_handler_program);
@@ -45,9 +48,13 @@ int main(void)
     pio_sm_set_enabled(pio, sm, true);
 
     while (true) {
-        //printf("%d\n",clock_get_hz(clk_sys));
         curr_signals = (pio_sm_get_blocking(pio, sm) >> shift_amount); // block until a new value.
-        if (curr_signals != prev_signals) {
+        // The below statement used to determine if the checker has changed.
+        checker_chg = (curr_signals & 0x7) != (prev_signals & 0x7);
+        // The below statement used to determine if any other signal after standbywfe has changed. 
+        rest_signals_chg = (curr_signals & 0xFFE0) != (prev_signals & 0xFFE0);
+
+        if (checker_chg || rest_signals_chg) {
             print_signals(curr_signals); 
             prev_signals = curr_signals;
         }
